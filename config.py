913b10396b1fc2f36f27b4e9cf6a94270fb36d43
AirMonk <vershkov.99@mail.ru>
2023-04-25 19:36:51 +0400

    Initinal commit

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '6080296043:AAFt9VViXcse2-CiT1XQTB1wYNpQyB-BBMg'
+
+URL = 'https://min-api.cryptocompare.com/data/price'
+
+KEYS = {
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'доллар': 'USD'
+}
